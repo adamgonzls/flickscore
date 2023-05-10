896d32e9749c84483fa324554af8136a52a1aab6
Adam Gonzales <adamgonzales1@gmail.com>
2023-05-10 13:01:32 -0600

    feature: add user input to search for movie, use The Movie Database API request to get movie data - Add routes to support searching for movie - Allow user to select movie from results - Add some CSS styling

--- addMovieForm.py
+++ addMovieForm.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 
 
 class AddMovieForm(FlaskForm):
-    review = StringField(label="Movie Title:")
+    title = StringField(label="Movie Title:")
     submit = SubmitField(label="Add Movie")
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from editMovieForm import EditMovieForm
 from addMovieForm import AddMovieForm
+import os
+import requests
 
 app = Flask(__name__)
 app.secret_key = "iwearmysunglassesatnight"
@@ -11,6 +13,10 @@ bootstrap = Bootstrap5(app)
 db = SQLAlchemy()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flickscore.db"
 db.init_app(app)
+
+TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
+TMDB_URL = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&language=en-US&query="
+
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
@@ -24,38 +30,42 @@ class Movie(db.Model):
 
 # new_movie = Movie(id=1, title="Moana", year=2016, rating=9.3, ranking=5, description="In Ancient Polynesia, when a terrible curse incurred by the Demigod Maui reaches Moana's island, she answers the Ocean's call to seek out the Demigod to set things right.", img_url="https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_.jpg")
 
+
 with app.app_context():
     db.create_all()
     # db.session.add(new_movie)
     # db.session.commit()
 
 
-movie_list = [{
-        "id": 1,
-        "title": "Matrix",
-        "rating": 5.5,
-        "description": "lots of words"
-    },
-    {
-        "id": 2,
-        "title": "Die Hard",
-        "rating": 9.5,
-        "description": "John McClain handles business!"
-    }]
-
-
 @app.route("/", methods=["GET", "POST"])
 def home():
     all_movies = db.session.query(Movie).all()
-    return render_template('index.html', movies=all_movies)
+    return render_template("index.html", movies=all_movies)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add_movie():
-    return render_template(("addMovie.html"))
+    add_movie_form = AddMovieForm()
+    if add_movie_form.validate_on_submit():
+        movie_title = add_movie_form.title.data
+        query = f"{movie_title}&page=1&include_adult=false"
+        movie_res = requests.get(f"{TMDB_URL}{query}")
+        movie_res.raise_for_status()
+        movies_json = movie_res.json()["results"]
+        print(movies_json)
+        return render_template("selectMovie.html", movies=movies_json)
+    return render_template("addMovie.html", form=add_movie_form)
 
 
-@app.route("/edit", methods=['GET', 'POST'])
+@app.route("/find")
+def find_movie():
+    movie_id = request.args.get("id")
+
+    print(f"The clicked id was: {movie_id}")
+    return "<p>This is the page</p>"
+
+
+@app.route("/edit", methods=["GET", "POST"])
 def edit_movie():
     movie_id = request.args.get("id")
     selected_movie = Movie.query.filter_by(id=movie_id).first()
@@ -70,17 +80,17 @@ def edit_movie():
         if new_review:
             movie_to_update.review = new_review
             db.session.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
     return render_template("editMovie.html", form=edit_movie_form, movie=selected_movie)
 
 
 @app.route("/delete")
 def delete_movie():
-    movie_id = request.args.get('id')
+    movie_id = request.args.get("id")
     movie_to_delete = Movie.query.get(movie_id)
     db.session.delete(movie_to_delete)
     db.session.commit()
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
