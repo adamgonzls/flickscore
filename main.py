59f2eb62185ec5912d49fd5ef90d5882358b81d7
Adam Gonzales <adamgonzales1@gmail.com>
2023-05-11 08:32:07 -0600

    feature: add ability for user to select movie from returned search results, then add review and rating after add to db - Update some API calls and update class Query to Session - Remove some not null requirements on movie entries to db - Update some HTML

--- main.py
+++ main.py
@@ -15,18 +15,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flickscore.db"
 db.init_app(app)
 
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
-TMDB_URL = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&language=en-US&query="
-
+TMDB_SEARCH_URL = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&language=en-US&query="
+TMDB_MOVIE_URL = f"https://api.themoviedb.org/3/movie/"
+TMDB_PARAMS = {"api_key": TMDB_API_KEY}
+TMDB_IMG_BASE = "https://image.tmdb.org/t/p/w500"
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     title = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
+    rating = db.Column(db.Float)
     ranking = db.Column(db.Integer)
     review = db.Column(db.String(250))
-    img_url = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250))
 
 # new_movie = Movie(id=1, title="Moana", year=2016, rating=9.3, ranking=5, description="In Ancient Polynesia, when a terrible curse incurred by the Demigod Maui reaches Moana's island, she answers the Ocean's call to seek out the Demigod to set things right.", img_url="https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_.jpg")
 
@@ -49,20 +51,33 @@ def add_movie():
     if add_movie_form.validate_on_submit():
         movie_title = add_movie_form.title.data
         query = f"{movie_title}&page=1&include_adult=false"
-        movie_res = requests.get(f"{TMDB_URL}{query}")
+        movie_res = requests.get(f"{TMDB_SEARCH_URL}{query}")
         movie_res.raise_for_status()
         movies_json = movie_res.json()["results"]
-        print(movies_json)
+        # print(movies_json)
         return render_template("selectMovie.html", movies=movies_json)
     return render_template("addMovie.html", form=add_movie_form)
 
 
 @app.route("/find")
 def find_movie():
-    movie_id = request.args.get("id")
-
-    print(f"The clicked id was: {movie_id}")
-    return "<p>This is the page</p>"
+    movie_id = request.args.get("movie_id")
+    edit_movie_form = EditMovieForm()
+    selected_movie_res = requests.get(url=f"{TMDB_MOVIE_URL}{movie_id}", params=TMDB_PARAMS)
+    selected_movie_res.raise_for_status()
+    selected_movie_json = selected_movie_res.json()
+    # print(selected_movie_json)
+    movie = Movie(
+        title=selected_movie_json['title'],
+        year=selected_movie_json['release_date'],
+        description=selected_movie_json['overview'],
+        img_url=f"{TMDB_IMG_BASE}{selected_movie_json['poster_path']}"
+    )
+    db.session.add(movie)
+    db.session.commit()
+    selected_movie = movie
+    return redirect(url_for("edit_movie", id=movie.id))
+    # return render_template("editMovie.html", form=edit_movie_form, movie=selected_movie)
 
 
 @app.route("/edit", methods=["GET", "POST"])
@@ -73,7 +88,7 @@ def edit_movie():
     if edit_movie_form.validate_on_submit():
         new_rating = edit_movie_form.rating.data
         new_review = edit_movie_form.review.data
-        movie_to_update = Movie.query.get(movie_id)
+        movie_to_update = db.session.get(Movie, movie_id)
         if new_rating:
             movie_to_update.rating = new_rating
             db.session.commit()
@@ -87,7 +102,7 @@ def edit_movie():
 @app.route("/delete")
 def delete_movie():
     movie_id = request.args.get("id")
-    movie_to_delete = Movie.query.get(movie_id)
+    movie_to_delete = db.session.get(Movie, movie_id)
     db.session.delete(movie_to_delete)
     db.session.commit()
     return redirect(url_for("home"))
